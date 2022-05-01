83d2cae565031e6b7fa3c1a385c56245fd873932
Siraj <sirajcp@gmail.com>
2022-05-01 23:59:03 +0100

    Added comments screen Added comments button and toggle button on post details screen

--- app/src/main/java/space/stanton/technicaltest/postdetails/PostDetailActivity.kt
+++ app/src/main/java/space/stanton/technicaltest/postdetails/PostDetailActivity.kt
@@ -3,14 +3,22 @@ package space.stanton.technicaltest.postdetails
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.CompoundButton
+import android.widget.Switch
 import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import dagger.hilt.android.AndroidEntryPoint
+import okhttp3.internal.filterList
+import space.stanton.technicaltest.Constants
 import space.stanton.technicaltest.R
-import space.stanton.technicaltest.model.PostItem
+import space.stanton.technicaltest.cache.PreferenceManager
+import space.stanton.technicaltest.comments.CommentsActivity
 import space.stanton.technicaltest.network.model.PostsItem
 import space.stanton.technicaltest.postlist.PostListViewModel
+
 
 /**
  * Shows details of a post
@@ -26,22 +34,46 @@ class PostDetailActivity : AppCompatActivity() {
         setContentView(R.layout.activity_post_details)
         val id = intent.getIntExtra("postId", 0)
         viewModel.postDetails.observe(this) {
-            updateUI(it.data)
+            updateUI(it.data!!)
         }
 
         viewModel.getPostsDetails(id)
 
     }
 
-    private fun updateUI(post: PostsItem?) {
+    private fun updateUI(post: PostsItem) {
         findViewById<TextView>(R.id.title).text = post?.title
         findViewById<TextView>(R.id.content).text = post?.body
         this@PostDetailActivity.title = post?.title
         findViewById<Button>(R.id.commentsBtn).setOnClickListener {
             startActivity(
-                Intent(this, PostDetailActivity::class.java)
+                Intent(this, CommentsActivity::class.java)
                     .putExtra("postId", post?.id)
             )
         }
+
+        val toggle = findViewById<Switch>(R.id.switch1)
+
+        toggle.setOnCheckedChangeListener(CompoundButton.OnCheckedChangeListener { buttonView, isChecked ->
+
+
+            val settings = PreferenceManager.customPrefs(this, Constants.PREF_NAME)
+            val postList = settings.getString("postToReadOffline", null)
+
+            if(postList != null) {
+                var posts: ArrayList<PostsItem> = Gson().fromJson(
+                    postList,
+                    object : TypeToken<ArrayList<PostsItem>>() {}.type
+                )
+
+                val newList: ArrayList<PostsItem> = posts.filter { it.id != post?.id }  as ArrayList<PostsItem>
+
+                if(isChecked) {
+                    newList.add(post)
+                }
+            }
+        })
+
+
     }
 }
